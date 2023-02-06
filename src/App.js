ebb1c71ec3e4934b7e729c072be27f99b7a5d9c1
Ritik Bhardwaj <112161046+hritikgo@users.noreply.github.com>
2023-02-06 11:39:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Add code to import the components
+import { AppProvider } from './context/AppContext';
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                //Add Budget component here under
+        
+                //Add Remaining component here under
+        
+                //Add ExpenseTotal component here under
+        
+                //Add ExpenseList component here under
+        
+                //Add ExpenseItem component here under
+        
+                //Add AllocationForm component here under
+
+            </div>
+        </AppProvider>
+    );
+};
+export default App;
